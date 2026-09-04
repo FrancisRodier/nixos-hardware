e240f218430164f3a4e77aef08b0b5af0b08afc0
Francis Rodier <francis@rodier.pro>
2026-09-04 15:11:05 +1000

    Adds support for HP Probook X360 Gen 8

---
+++ hp/probook/x360G8/default.nix
@@ -0,0 +1,28 @@
+{ pkgs, lib, ... }:
+
+{
+  imports = [
+    ../../../common/cpu/amd
+    ../../../common/cpu/amd/pstate.nix
+    ../../../common/gpu/amd
+    ../../../common/pc/laptop
+    ../../../common/pc/ssd
+  ];
+
+  boot.kernelModules = [ "synaptics_usb" ];
+  boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.3") (
+    lib.mkDefault pkgs.linuxPackages_latest
+  );
+
+  # disable Scatter/Gather APU recently enabled by default,
+  # which results in white screen after display reconfiguration
+  boot.kernelParams = [ "amdgpu.sg_display=0" ];
+
+  services.xserver = {
+    videoDrivers = [ "amdgpu" ];
+  };
+    hardware = {
+      enableRedistributableFirmware = lib.mkDefault true;
+      sensor.iio.enable = lib.mkDefault true; # support orientation and ambient light sensors
+    };
+}
